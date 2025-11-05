94a4165da64a8fa393e0b17ce0b3956ccf02a127
AbhishekSonkrr <129600966+AbhishekSonkrr@users.noreply.github.com>
2025-11-05 16:57:48 +0530

    Remove unused sorting files and add assignment 8 source files and outputs

--- assignment-8/q3.c
+++ assignment-8/q3.c
@@ -3,7 +3,7 @@
 #include <errno.h>
 
 int main(void) {
-    for (int i = 0; i < 25; i++) {
+    for (int i = 0; i < 20; i++) {
         const char* msg = strerror(i);
         if (strcmp(msg, "Unknown error") != 0) {
             printf("Error %d: %s\n", i, msg);
